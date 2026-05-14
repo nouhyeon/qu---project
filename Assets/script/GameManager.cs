e2ffbca23c4b4511a3cb4982b630ce0c3ffbdd6f
aide <90443112+Aiied@users.noreply.github.com>
2026-05-14 20:29:13 +0900

    26-05-14 sprite
    Main ui 이미지 적용

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -5,10 +5,17 @@ public class GameManager : MonoBehaviour
     public int count;
     public int coin;
 
+    public int sceneNum;
+
+    bool clear;
+
+
+
     public GameObject pause;
     void Start()
     {
         count = coin = 0;
+        clear = false;
     }
 
     void Update()
